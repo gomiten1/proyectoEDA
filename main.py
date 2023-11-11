51555cfa00d48e8091cd6e5b5f7679b79f8b8001
gomiten1 <masmibe@gmail.com>
2023-11-11 15:43:54 -0600

    Disenio inicial

--- main.py
+++ main.py
@@ -42,24 +42,55 @@ def login():
 
 def signup():
     pass
+
+def administer_trips():
+    pass
+
+def search_elements():
+    pass
+
+def registerElements():
+    menu = int(input("!. Cliente 2. Scooter 3. Station"))
+    if menu == 1:
+        pass
+    elif menu == 2:
+        pass
+    elif menu == 3:
+        pass
+    else:
+        print("Please, choose a valid option")
     
 if __name__ == "__main__":
     while True:
+        while True:
+            
+            print("\n\nWelcome, please select one of the options")
+            menuLogin = int(input("1. Create account 2. Log in"))
+            
+            if menuLogin == 1:
+                signup()
+                break
+            elif menuLogin == 2:
+                user = login()
+                break
+            else:
+                print("Please input valid option")
         
-        print("\n\nWelcome, please select one of the options")
-        menuLogin = int(input("1. Create account 2. Log in"))
+        print("Login successful!")
         
-        if menuLogin == 1:
-            signup()
-            break
-        elif menuLogin == 2:
-            user = login()
-            break
-        else:
-            print("Please input valid option")
-    
-    print("Login successful!")
-    
-    
+        while True:
+        
+            menuOption = int(input("1. Register elements 2. Administer trips 3. Search elements 4. Exit"))
+            
+            
+            if menuOption == 1: 
+                registerElements()
+            elif menuOption == 2:
+                administer_trips()
+            elif menuOption == 3:
+                search_elements()
+            elif menuOption == 4:
+                break
+        
     
     
